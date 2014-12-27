1e5033ab9cdfd53a1b7c88afc51cdb2df64c815f
Umur Gedik <umurgdk@gmail.com>
2014-12-27 18:28:08 +0200

    Activity deserializer shrinked

--- app/src/main/java/org/beraber/beraber/repositories/ActivityDeserializer.java
+++ app/src/main/java/org/beraber/beraber/repositories/ActivityDeserializer.java
@@ -1,5 +1,7 @@
 package org.beraber.beraber.repositories;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -10,43 +12,26 @@ import org.beraber.beraber.daos.Activity;
 import org.beraber.beraber.daos.User;
 
 import java.lang.reflect.Type;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class ActivityDeserializer implements JsonDeserializer<Activity> {
 
     @Override
     public Activity deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd")
+                .create();
+
+
+        Activity activity = gson.fromJson(json, Activity.class);
+
         JsonObject obj = json.getAsJsonObject();
+        if (obj.has("user")) {
+            JsonObject objUser = obj.getAsJsonObject("user");
+            User user = gson.fromJson(objUser, User.class);
 
-        try {
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-            Date result = df.parse(obj.get("start_date").getAsString());
-
-            Activity activity = new Activity(
-                    obj.get("id").getAsLong(),
-                    obj.get("title").getAsString(),
-                    obj.get("description").getAsString(),
-                    result,
-                    obj.get("user_id").getAsLong());
-
-            if (obj.has("user")) {
-                JsonObject objUser = obj.getAsJsonObject("user");
-                User user = new User(
-                        objUser.get("id").getAsLong(),
-                        objUser.get("name").getAsString(),
-                        objUser.get("bio").getAsString()
-                );
-
-                activity.setUser(user);
-            }
-
-            return activity;
-
-        } catch (ParseException e) {
-            throw new JsonParseException(e.getMessage());
+            activity.setUser(user);
         }
+
+        return activity;
     }
 }
